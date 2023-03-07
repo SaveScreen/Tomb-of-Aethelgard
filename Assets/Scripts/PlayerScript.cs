86bc6c384f02e23cb3d6237193823c9987bce799
Saving Progress <97535452+SaveScreen@users.noreply.github.com>
2023-03-06 22:15:49 -0500

    Added rotation to the player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    //Movement variables
     public InputAction playermove;
     private CharacterController charactercontroller;
     public float speed;
@@ -14,25 +15,43 @@ public class PlayerScript : MonoBehaviour
     private Vector3 velocity;
     private float gravity;
 
+    //Looking variables
+    public InputAction playerrotate;
+    public float sensitivity;
+    private Vector2 look;
+    private float lookx;
+    private float looky;
+    private float rotationx;
+    private float rotationy;
+    public Transform orientation;
+
+    
     // Start is called before the first frame update
     void Start()
     {
         charactercontroller = gameObject.GetComponent<CharacterController>();
         gravity = -9.81f;
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
+
     private void OnEnable()
     {
         playermove.Enable();
+        playerrotate.Enable();
     }
 
     private void OnDisable()
     {
         playermove.Disable();
+        playerrotate.Disable();
     }
     // Update is called once per frame
     void Update()
     {
         move = playermove.ReadValue<Vector3>();
+        Look();
     }
 
     private void FixedUpdate()
@@ -43,5 +62,20 @@ public class PlayerScript : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
         charactercontroller.Move(velocity * Time.deltaTime);
         
+    }
+
+    //Controls player looking around
+    private void Look() {
+        look = playerrotate.ReadValue<Vector2>();
+
+        lookx = look.x * sensitivity * Time.deltaTime;
+        looky = look.y * sensitivity * Time.deltaTime;
+
+        rotationx -= looky;
+        rotationy += lookx;
+
+        rotationx = Mathf.Clamp(rotationx,-90.0f,90.0f);
+        transform.rotation = Quaternion.Euler(-rotationx,-rotationy,0);
+        orientation.transform.Rotate(Vector3.up * lookx);
     }
 }

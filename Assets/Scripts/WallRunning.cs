1c65b2ca68cf5107fd7f12eff43f798d33d5e21f
treemanrocket <albert52798@knights.ucf.edu>
2023-03-08 16:49:00 -0500

    Update on the wall run script

--- Assets/Scripts/WallRunning.cs
+++ Assets/Scripts/WallRunning.cs
@@ -21,6 +21,8 @@ public class WallRunning : MonoBehaviour
     public float MinJumpHeight;
     private RaycastHit LeftWall;
     private RaycastHit RightWall;
+    private bool WallIsRight;
+    private bool WallIsLeft;
 
     [Header("refrences")]
     public Transform orientation;
@@ -36,6 +38,33 @@ public class WallRunning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckForWall();
+    }
+
+
+    private void CheckForWall() 
+    {
+        WallIsRight = Physics.Raycast(transform.position, orientation.right, out RightWall, WallCheckDistance, IsThisWall);
+      
+        WallIsLeft = Physics.Raycast(transform.position, -orientation.right, out LeftWall, WallCheckDistance, IsThisWall);
         
+    }
+    private bool AboveGround()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, MinJumpHeight, IsThisGround);
+    }
+
+    private void StateMachine()
+    {
+        //grabs the input from the old inputs, needs more testing if there will be complexcations
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+
+        //state 1 - wall running
+        if ((WallIsRight || WallIsLeft) && verticalInput > 0 && AboveGround())
+        {
+            //wallrunning script here
+        }
     }
 }

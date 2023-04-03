534bbe07e83a25fe3ea8913cf19ae878fede2fec
Ethan Branham <et814335@knights.ucf.edu>
2023-04-03 16:39:54 -0400

    Main Menu with controls

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -5,6 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    private GameObject mainmenu;
+    public GameObject controlsmenu;
+
+    void Awake()
+    {
+        mainmenu = Instantiate(controlsmenu);
+        mainmenu.transform.SetParent(transform);
+        controlsmenu.SetActive(false);
+    }
+    public void ShowControls()
+    {
+        controlsmenu.SetActive(true);
+    }
+
+    public void HideControls()
+    {
+        controlsmenu.SetActive(false);
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //takes current scene and moves to next scene in scene order
